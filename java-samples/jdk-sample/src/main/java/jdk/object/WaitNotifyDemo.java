2f8f6ff82373e75ca7e1e0ae2011790b63221340
yx91490 <yx91490@126.com>
2020-05-23 09:18:50 +0800

    add WaitNotifyDemo

---
+++ java-samples/jdk-sample/src/main/java/jdk/object/WaitNotifyDemo.java
@@ -0,0 +1,43 @@
+package jdk.object;
+
+public class WaitNotifyDemo {
+
+    static Object lock = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        new T1().start();
+        new T2().start();
+    }
+
+    public static class T1 extends Thread {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                System.out.println(System.currentTimeMillis() + ":T1 start!");
+                try {
+                    System.out.println(System.currentTimeMillis() + ":T1 wait for object");
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(System.currentTimeMillis() + ":T1 end!");
+            }
+        }
+    }
+
+    public static class T2 extends Thread {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                System.out.println(System.currentTimeMillis() + ":T2 start，notify one thread! ");
+                lock.notify();
+                System.out.println(System.currentTimeMillis() + ":T2 end!");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
