306d0f387acb4d4a02a0fad4d0fc0755c4f43b0f
yx91490 <yx91490@126.com>
2020-05-24 16:42:14 +0800

    add DaemonDemo

---
+++ java-samples/jdk-sample/src/main/java/jdk/thread/DaemonDemo.java
@@ -0,0 +1,19 @@
+package jdk.thread;
+
+public class DaemonDemo {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread("子线程1") {
+            @Override
+            public void run() {
+                System.out.println(this.getName() +
+                        "开始执行," +
+                        (this.isDaemon() ? "我是守护线程" : "我是用户线程"));
+                while (true) ;
+            }
+        };
+        t1.setDaemon(true);
+        t1.start();
+        System.out.println("主线程结束");
+    }
+}
