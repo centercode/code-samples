e518a780791bf8f8d51b9abd87ef8ef58c7bb344
yx91490 <yx91490@126.com>
2020-06-14 18:39:00 +0800

    add ReentrantLockDemos

---
+++ java-samples/jdk-sample/src/main/java/jdk/lock/ReentryVerifyDemo.java
@@ -0,0 +1,47 @@
+package jdk.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 可重入性验证
+ */
+public class ReentryVerifyDemo {
+    private static int num = 0;
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    private static void add() {
+        lock.lock();
+        lock.lock();
+        try {
+            num++;
+        } finally {
+            lock.unlock();
+            lock.unlock();
+        }
+    }
+
+    public static class T extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                ReentryVerifyDemo.add();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        T t1 = new T();
+        T t2 = new T();
+        T t3 = new T();
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println(num);
+    }
+}
