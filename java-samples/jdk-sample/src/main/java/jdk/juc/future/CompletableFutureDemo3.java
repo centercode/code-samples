bca109fc1c54d64a3831e2ff57a659fd6d70ea2d
yx91490 <yx91490@126.com>
2020-08-06 08:56:01 +0800

    add CompletableFutureDemos

---
+++ java-samples/jdk-sample/src/main/java/jdk/juc/future/CompletableFutureDemo3.java
@@ -0,0 +1,21 @@
+package jdk.juc.future;
+
+import jdk.util.Threads;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class CompletableFutureDemo3 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            Threads.silentSleep(5, TimeUnit.SECONDS);
+            return 5;
+        }).applyToEither(CompletableFuture.supplyAsync(() -> {
+            Threads.silentSleep(3, TimeUnit.SECONDS);
+            return 3;
+        }), f -> f * 10).whenComplete((r, tx) -> System.out.println(r));
+
+        future.get();
+    }
+}
