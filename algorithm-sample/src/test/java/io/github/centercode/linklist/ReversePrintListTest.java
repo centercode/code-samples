3480eb082ac84d58003e187f42841f7c6885f185
yx91490 <yx91490@126.com>
2020-08-27 07:11:27 +0800

    add ReversePrintList

---
+++ algorithm-sample/src/test/java/io/github/centercode/linklist/ReversePrintListTest.java
@@ -0,0 +1,22 @@
+package io.github.centercode.linklist;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class ReversePrintListTest {
+
+    @Test
+    public void solution1() {
+        ReversePrintList.ListNode n1 = new ReversePrintList.ListNode(1);
+        ReversePrintList.ListNode n2 = new ReversePrintList.ListNode(3);
+        ReversePrintList.ListNode n3 = new ReversePrintList.ListNode(2);
+
+        n1.next = n2;
+        n2.next = n3;
+
+        ReversePrintList reversePrintList = new ReversePrintList();
+        int[] arr = reversePrintList.solution1(n1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
