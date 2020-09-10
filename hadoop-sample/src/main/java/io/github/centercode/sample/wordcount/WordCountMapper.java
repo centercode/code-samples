9671e1b2df4576a54bdcd5555d747a214edb3aa3
yx91490 <yx91490@126.com>
2020-09-10 20:03:29 +0800

    update WordCountMapper

--- hadoop-sample/src/main/java/io/github/centercode/sample/wordcount/WordCountMapper.java
+++ hadoop-sample/src/main/java/io/github/centercode/sample/wordcount/WordCountMapper.java
@@ -9,6 +9,12 @@ import java.io.IOException;
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
     @Override
+    protected void setup(Context context) {
+//        Configuration configuration = context.getConfiguration();
+//        configuration.getInt("word.count.ignored.length", 3);
+    }
+
+    @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String[] words = line.split(" ");
