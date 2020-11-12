a27cc4102615ca3d8127b3e5283e27ae195b21a6
yx91490 <yx91490@126.com>
2020-11-12 15:23:12 +0800

    rename

--- java-sample/jdk-sample/src/main/java/io/IOUtils.java
+++ java-sample/jdk-sample/src/main/java/io/IOUtils.java
@@ -5,10 +5,10 @@ import java.nio.charset.StandardCharsets;
 
 public class IOUtils {
 
-    public static String readString(InputStream input) throws IOException {
-        InputStreamReader in = new InputStreamReader(input, StandardCharsets.UTF_8);
+    public static String readString(InputStream in) throws IOException {
+        InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
         StringBuilder sw = new StringBuilder();
-        copy(in, sw);
+        copy(reader, sw);
         return sw.toString();
     }
 
@@ -31,22 +31,22 @@ public class IOUtils {
         return count;
     }
 
-    public static long copy(Reader in, Writer out) throws IOException {
+    public static long copy(Reader reader, Writer out) throws IOException {
         char[] buffer = new char[1 << 12];
         long count = 0;
         int n;
-        while ((n = in.read(buffer)) != -1) {
+        while ((n = reader.read(buffer)) != -1) {
             out.write(buffer, 0, n);
             count += n;
         }
         return count;
     }
 
-    public static long copy(Reader in, StringBuilder out) throws IOException {
+    public static long copy(Reader reader, StringBuilder out) throws IOException {
         char[] buffer = new char[1 << 12];
         long count = 0;
         int n;
-        while ((n = in.read(buffer)) != -1) {
+        while ((n = reader.read(buffer)) != -1) {
             out.append(buffer, 0, n);
             count += n;
         }
