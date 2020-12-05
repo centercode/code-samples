89d429208b520765e76efd8ff238449374a1d4b7
yx91490 <yx91490@126.com>
2020-12-05 12:04:38 +0800

    add jackson-sample module

---
+++ jackson-sample/src/main/java/io/github/centercode/XmlArrayParserSample.java
@@ -0,0 +1,48 @@
+package io.github.centercode;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class XmlArrayParserSample {
+
+    private static final XmlMapper mapper = new XmlMapper();
+
+    static {
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        URI uri = ClassLoader.getSystemResource("hadoop.xml").toURI();
+        String xmlStr = new String(Files.readAllBytes(Paths.get(uri)), UTF_8);
+        Configuration conf = mapper.readValue(xmlStr, Configuration.class);
+        for (Property pair : conf.properties) {
+            String name = pair.name;
+            String value = pair.value;
+            System.out.println(pair.source + "\t" + name + "=" + value);
+        }
+    }
+
+    static class Configuration {
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "property")
+        private List<Property> properties;
+    }
+
+    static class Property {
+        public String name;
+        public String value;
+        public String source;
+    }
+}
