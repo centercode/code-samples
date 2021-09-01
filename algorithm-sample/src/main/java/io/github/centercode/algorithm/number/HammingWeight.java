6ecfab57297f6bce443f1936b8520f671848771d
yx91490 <yx91490@126.com>
2021-09-01 09:18:01 +0800

    algorithm: add HammingWeight

---
+++ algorithm-sample/src/main/java/io/github/centercode/algorithm/number/HammingWeight.java
@@ -0,0 +1,18 @@
+package io.github.centercode.algorithm.number;
+
+/**
+ * 191. 位1的个数
+ * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+ */
+public class HammingWeight {
+
+    // you need to treat n as an unsigned value
+    public int solution(int n) {
+        int count = 0;
+        while (n != 0) {
+            n &= n - 1;
+            count++;
+        }
+        return count;
+    }
+}
