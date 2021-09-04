08f496b6ae4384c889ecc07789c21da0d6f50859
yx91490 <yx91490@126.com>
2021-09-04 19:04:14 +0800

    algorithm: add isBalancedBiTree

---
+++ algorithm-sample/src/main/java/io/github/centercode/algorithm/tree/isBalancedBiTree.java
@@ -0,0 +1,26 @@
+package io.github.centercode.algorithm.tree;
+
+/**
+ * 剑指 Offer 55 - II. 平衡二叉树
+ * 如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+ * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。
+ */
+public class isBalancedBiTree {
+
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
