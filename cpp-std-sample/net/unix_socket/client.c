8582e4106ed6225da919075a6131ebc7eb4ee065
yx91490 <yx91490@126.com>
2022-11-27 21:15:04 +0800

    update cpp-std-sample: add unix_socket sample

---
+++ cpp-std-sample/net/unix_socket/client.c
@@ -0,0 +1,58 @@
+/* https://www.cnblogs.com/sparkdev/p/8359028.html */
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define MAXLINE 80
+
+char *client_path = "client.socket";
+char *server_path = "server.socket";
+
+int main() {
+  struct sockaddr_un cliun, serun;
+  int len;
+  char buf[100];
+  int sockfd, n;
+
+  if ((sockfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+    perror("client socket error");
+    exit(1);
+  }
+
+  // 一般显式调用bind函数，以便服务器区分不同客户端
+  memset(&cliun, 0, sizeof(cliun));
+  cliun.sun_family = AF_UNIX;
+  strcpy(cliun.sun_path, client_path);
+  len = offsetof(struct sockaddr_un, sun_path) + strlen(cliun.sun_path);
+  unlink(cliun.sun_path);
+  if (bind(sockfd, (struct sockaddr *)&cliun, len) < 0) {
+    perror("bind error");
+    exit(1);
+  }
+
+  memset(&serun, 0, sizeof(serun));
+  serun.sun_family = AF_UNIX;
+  strcpy(serun.sun_path, server_path);
+  len = offsetof(struct sockaddr_un, sun_path) + strlen(serun.sun_path);
+  if (connect(sockfd, (struct sockaddr *)&serun, len) < 0) {
+    perror("connect error");
+    exit(1);
+  }
+
+  while (fgets(buf, MAXLINE, stdin) != NULL) {
+    write(sockfd, buf, strlen(buf));
+    n = read(sockfd, buf, MAXLINE);
+    if (n < 0) {
+      printf("the other side has been closed.\n");
+    } else {
+      write(STDOUT_FILENO, buf, n);
+    }
+  }
+  close(sockfd);
+  return 0;
+}
---
+++ cpp-std-sample/net/unix_socket/server.c
@@ -0,0 +1,74 @@
+/* https://www.cnblogs.com/sparkdev/p/8359028.html */
+#include <ctype.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define MAXLINE 80
+
+char *socket_path = "server.socket";
+
+int main(void) {
+  struct sockaddr_un serun, cliun;
+  socklen_t cliun_len;
+  int listenfd, connfd, size;
+  char buf[MAXLINE];
+  int i, n;
+
+  if ((listenfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+    perror("socket error");
+    exit(1);
+  }
+
+  memset(&serun, 0, sizeof(serun));
+  serun.sun_family = AF_UNIX;
+  strcpy(serun.sun_path, socket_path);
+  size = offsetof(struct sockaddr_un, sun_path) + strlen(serun.sun_path);
+  unlink(socket_path);
+  if (bind(listenfd, (struct sockaddr *)&serun, size) < 0) {
+    perror("bind error");
+    exit(1);
+  }
+  printf("UNIX domain socket bound\n");
+
+  if (listen(listenfd, 20) < 0) {
+    perror("listen error");
+    exit(1);
+  }
+  printf("Accepting connections ...\n");
+
+  while (1) {
+    cliun_len = sizeof(cliun);
+    if ((connfd = accept(listenfd, (struct sockaddr *)&cliun, &cliun_len)) <
+        0) {
+      perror("accept error");
+      continue;
+    }
+
+    while (1) {
+      n = read(connfd, buf, sizeof(buf));
+      if (n < 0) {
+        perror("read error");
+        break;
+      } else if (n == 0) {
+        printf("EOF\n");
+        break;
+      }
+
+      printf("received: %s", buf);
+
+      for (i = 0; i < n; i++) {
+        buf[i] = toupper(buf[i]);
+      }
+      write(connfd, buf, n);
+    }
+    close(connfd);
+  }
+  close(listenfd);
+  return 0;
+}
