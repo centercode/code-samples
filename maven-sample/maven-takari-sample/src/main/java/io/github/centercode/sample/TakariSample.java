2a4ae6b6fd1e28af5cff0899ff4c1e9fe57511d4
yx91490 <yx91490@126.com>
2023-05-08 23:02:28 +0800

    update maven-sample: add module 'maven-takari-sample'

---
+++ maven-sample/maven-takari-sample/src/main/java/io/github/centercode/sample/TakariSample.java
@@ -0,0 +1,11 @@
+package io.github.centercode.sample;
+
+import lombok.Data;
+
+@Data
+public class TakariSample {
+
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
