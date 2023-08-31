a8fc1e4728697feb90b318d3433dd121badaafc1
yx91490 <yx91490@126.com>
2023-08-31 08:19:58 +0800

    update algorithm-sample: add FindContinuousSequence.java

---
+++ algorithm-sample/src/main/java/io/github/centercode/algorithm/dp/FindContinuousSequence.java
@@ -0,0 +1,54 @@
+package io.github.centercode.algorithm.dp;
+
+import java.util.ArrayList;
+
+/**
+ * 剑指 Offer 57 - II. 和为s的连续正数序列
+ */
+public class FindContinuousSequence {
+
+    public int[][] solution1(int target) {
+        ArrayList<int[]> result = new ArrayList<>();
+        for (int n = target / 2; n > 1; n--) {
+            if (n * (n + 1) > 2 * target) {
+                continue;
+            }
+            // x: 左边界
+            int x = (target - n * (n - 1) / 2) / n;
+            if (x > 0 && (2 * x + n - 1) * n == target * 2) {
+                result.add(fill(x, n));
+            }
+        }
+        return result.toArray(new int[0][0]);
+    }
+
+    int[] fill(int start, int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = start + i;
+        }
+        return arr;
+    }
+
+    /**
+     * 滑动窗口（双指针法）
+     * tc: O(n) sc: O(1)
+     */
+    public int[][] solution2(int target) {
+        ArrayList<int[]> result = new ArrayList<>();
+        int i = 1, j = 2;
+        while (i < j) {
+            int sum = (i + j) * (j - i + 1) / 2;
+            if (sum < target) {
+                j++;
+            } else if (sum == target) {
+                result.add(fill(i, j - i + 1));
+                i++;
+            } else {
+                i++;
+            }
+        }
+        return result.toArray(new int[0][]);
+    }
+
+}
---
+++ algorithm-sample/src/test/java/io/github/centercode/algorithm/number/FindContinuousSequenceTest.java
@@ -0,0 +1,20 @@
+package io.github.centercode.algorithm.number;
+
+import io.github.centercode.algorithm.dp.FindContinuousSequence;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindContinuousSequenceTest {
+
+    @Test
+    public void solution1() {
+        int[][] expectResult = {
+                {1, 2, 3, 4, 5},
+                {4, 5, 6},
+                {7, 8}
+        };
+        FindContinuousSequence findContinuousSequence = new FindContinuousSequence();
+        int[][] result = findContinuousSequence.solution2(15);
+        Assert.assertArrayEquals(result, expectResult);
+    }
+}
