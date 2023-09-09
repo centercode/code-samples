cc1cc316b4f44f510a73c6ad1c8234eb0e0cc9f5
yx91490 <yx91490@126.com>
2023-09-09 14:18:53 +0800

    update algorithm-sample: update SingleNumbers.java

--- algorithm-sample/src/main/java/io/github/centercode/algorithm/number/SingleNumbers.java
+++ algorithm-sample/src/main/java/io/github/centercode/algorithm/number/SingleNumbers.java
@@ -8,7 +8,7 @@ public class SingleNumbers {
     /**
      * tc: O(n) sc:O(1)
      */
-    public int[] solution1(int[] nums) {
+    public int[] case1Solution1(int[] nums) {
         int x = 0;
         for (int n : nums) {
             x ^= n;
@@ -27,5 +27,34 @@ public class SingleNumbers {
             }
         }
         return new int[]{a, b};
+    }
+
+    public int case2Solution1(int[] nums) {
+        int[] sum = new int[32];
+        for (int n : nums) {
+            for (int i = 0; i < 32; i++) {
+                sum[i] += n & 0x1;
+                n >>>= 1;
+            }
+        }
+        int r = 0;
+        for (int i = 0; i < 32; i++) {
+            r ^= (sum[i] % 3) << i;
+        }
+//        // 或者
+//        for (int i = 0; i < 32; i++) {
+//            r <<= 1;
+//            r |= sum[31 - i] % 3;
+//        }
+        return r;
+    }
+
+    public int case2Solution2(int[] nums) {
+        int ones = 0, twos = 0;
+        for (int num : nums) {
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
     }
 }
--- algorithm-sample/src/test/java/io/github/centercode/algorithm/number/SingleNumbersTest.java
+++ algorithm-sample/src/test/java/io/github/centercode/algorithm/number/SingleNumbersTest.java
@@ -5,19 +5,23 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
-
 public class SingleNumbersTest {
+    SingleNumbers singleNumbers = new SingleNumbers();
 
     @Test
-    public void solution1() {
-        SingleNumbers singleNumbers = new SingleNumbers();
-        int[] results = singleNumbers.solution1(new int[]{4, 1, 4, 6});
+    public void case1Solution1() {
+        int[] results = singleNumbers.case1Solution1(new int[]{4, 1, 4, 6});
         Arrays.sort(results);
         Assert.assertArrayEquals(new int[]{1, 6}, results);
 
-        results = singleNumbers.solution1(new int[]{1, 2, 10, 4, 1, 4, 3, 3});
+        results = singleNumbers.case1Solution1(new int[]{1, 2, 10, 4, 1, 4, 3, 3});
         Arrays.sort(results);
         Assert.assertArrayEquals(new int[]{2, 10}, results);
+    }
+
+    @Test
+    public void case2Solution1() {
+        int[] nums = {9, 1, 7, 9, 7, 9, 7};
+        Assert.assertEquals(1, singleNumbers.case2Solution1(nums));
     }
 }
