29c09cd727405afa6015ac5458bb8b754c86cf30
yx91490 <yx91490@126.com>
2023-09-09 14:17:39 +0800

    update algorithm-sample: update LevelTraverse.java

--- algorithm-sample/src/main/java/io/github/centercode/algorithm/tree/LevelTraverse.java
+++ algorithm-sample/src/main/java/io/github/centercode/algorithm/tree/LevelTraverse.java
@@ -35,6 +35,10 @@ class LevelTraverse {
         return res;
     }
 
+    /**
+     * 剑指 Offer 32 - II. 从上到下打印二叉树 II
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+     */
     public List<List<Integer>> case2Solution1(TreeNode root) {
         if (root == null) {
             return Collections.emptyList();
@@ -57,5 +61,38 @@ class LevelTraverse {
             result.add(segment);
         }
         return result;
+    }
+
+    /**
+     * 请实现一个函数按照之字形顺序打印二叉树，
+     * 即第一行按照从左到右的顺序打印，
+     * 第二层按照从右到左的顺序打印，
+     * 第三行再按照从左到右的顺序打印，其他行以此类推。
+     */
+    public List<List<Integer>> case3solution1(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                if (res.size() % 2 == 0) {
+                    tmp.addLast(node.val); // 偶数层 -> 队列头部
+                } else {
+                    tmp.addFirst(node.val); // 奇数层 -> 队列尾部
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
     }
 }
--- algorithm-sample/src/test/java/io/github/centercode/algorithm/tree/LevelTraverseTest.java
+++ algorithm-sample/src/test/java/io/github/centercode/algorithm/tree/LevelTraverseTest.java
@@ -3,7 +3,12 @@ package io.github.centercode.algorithm.tree;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LevelTraverseTest {
+
+    LevelTraverse levelTraverse = new LevelTraverse();
 
     @Test
     public void solution1() {
@@ -12,12 +17,22 @@ public class LevelTraverseTest {
         TreeNode n20 = new TreeNode(20);
         TreeNode n15 = new TreeNode(15);
         TreeNode n7 = new TreeNode(7);
-        n3.left =n9;
-        n3.right =n20;
-        n20.left =n15;
-        n20.right=n7;
-        LevelTraverse levelTraverse = new LevelTraverse();
+        n3.left = n9;
+        n3.right = n20;
+        n20.left = n15;
+        n20.right = n7;
         int[] result = levelTraverse.case1Solution1(n3);
-        Assert.assertArrayEquals(new int[] {3,9,20,15,7}, result);
+        Assert.assertArrayEquals(new int[]{3, 9, 20, 15, 7}, result);
+    }
+
+    @Test
+    public void case3solution1() {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        List<List<Integer>> result = levelTraverse.case3solution1(root);
+        System.out.println(result);
     }
 }
