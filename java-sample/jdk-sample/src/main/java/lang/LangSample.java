571641b0394f8363fed4d26775154ccced0357c3
yx91490 <yx91490@126.com>
2023-10-17 10:48:08 +0800

    update jdk-sample: update LangSample: add shift operator sample

--- java-sample/jdk-sample/src/main/java/lang/LangSample.java
+++ java-sample/jdk-sample/src/main/java/lang/LangSample.java
@@ -1,23 +1,7 @@
 package lang;
 
+/**
+ * see: LangSampleTest
+ */
 public class LangSample {
-    public static void main(String[] args) {
-        LangSample sample = new LangSample();
-        sample.equal();
-    }
-
-    void equal() {
-        Integer a = 12;
-        Integer b = 12;
-        int c = 12;
-        System.out.println(a == b); // true
-        System.out.println(b == c); // true
-
-        Double d1 = 1.0d;
-        Double d2 = 1.0d;
-        double d3 = 1.0d;
-        double d4 = 1.0d;
-        System.out.println(d1 == d2); // false
-        System.out.println(d3 == d4); // true
-    }
 }
---
+++ java-sample/jdk-sample/src/test/java/lang/LangSampleTest.java
@@ -0,0 +1,77 @@
+package lang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LangSampleTest {
+
+    @Test
+    public void testNumericEqual() {
+        Integer a = 12;
+        Integer b = 12;
+        int c = 12;
+        Assert.assertTrue(a == b); // true
+        Assert.assertTrue(b == c); // true
+
+        Double d1 = 1.0d;
+        Double d2 = 1.0d;
+        double d3 = 1.0d;
+        double d4 = 1.0d;
+        Assert.assertTrue(d1 != d2); // false
+        Assert.assertTrue(d3 == d4); // true
+    }
+
+    @Test
+    public void testMinusOperator() {
+        assertAllEquals(-0xFFFFFFFF, 1);
+        assertAllEquals(-0x80000000, 0x80000000);
+    }
+
+    @Test
+    public void testShiftOperator() {
+        // 左移
+        assertAllEquals(0 << 1, 0);
+        assertAllEquals(1 << 1, 2);
+        assertAllEquals(0B1101 << 1, 0B11010);
+        // 1073741824 << 1 => -2147483648
+        assertAllEquals(
+            0B0100_0000_0000_0000_0000_0000_0000_0000 << 1,
+            0B1000_0000_0000_0000_0000_0000_0000_0000,
+            -2147483648,
+            Integer.MIN_VALUE
+        );
+        assertAllEquals(0B0100_0000_0000_0000_0000_0000_0000_0000 << 2, 0);
+        // 2147483647 << 1 => -4294967294 => -2
+        assertAllEquals(Integer.MAX_VALUE << 1, 0xFFFFFFFE, -2);
+        assertAllEquals(Integer.MAX_VALUE << 2, 0xFFFFFFFC, -4);
+        assertAllEquals(Integer.MAX_VALUE << 3, 0xFFFFFFF8, -8);
+
+        // 带符号右移
+        assertAllEquals(0 >> 1, 0);
+        assertAllEquals(1 >> 1, 0);
+        assertAllEquals(0B1101 >> 1, 0B110);
+        assertAllEquals(Integer.MIN_VALUE >> 1, -0x40000000);
+        assertAllEquals(0xFFFFFFFF >> 1, 0xFFFFFFFF, -1);
+        assertAllEquals(0xFFFFFFFF >> 2, 0xFFFFFFFF, -1);
+        assertAllEquals(0xFFFFFFFF >> 3, 0xFFFFFFFF, -1);
+
+        // 无符号右移
+        assertAllEquals(0 >>> 1, 0);
+        assertAllEquals(1 >>> 1, 0);
+        assertAllEquals(0B1101 >>> 1, 0B110);
+        assertAllEquals(Integer.MIN_VALUE >>> 1, 0x40000000);
+        assertAllEquals(0xFFFFFFFF >>> 1, 0x7FFFFFFF, 2147483647, Integer.MAX_VALUE);
+        assertAllEquals(0xFFFFFFFF >>> 2, 0x3FFFFFFF, 1073741823);
+        assertAllEquals(0xFFFFFFFF >>> 3, 0x1FFFFFFF, 536870911);
+    }
+
+    boolean assertAllEquals(int... nums) {
+        if (nums.length == 0) {
+            return true;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            Assert.assertTrue(nums[0] == nums[i]);
+        }
+        return true;
+    }
+}
