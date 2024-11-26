c768fea2c76a0bdf0a998b29524b84a1bf0b6d6f
yx91490 <yx91490@126.com>
2024-11-26 11:53:58 +0800

    update jdk-sample: add Hex conversion.

--- java-sample/jdk-sample/src/test/java/lang/LangSampleTest.java
+++ java-sample/jdk-sample/src/test/java/lang/LangSampleTest.java
@@ -35,10 +35,10 @@ public class LangSampleTest {
         assertAllEquals(0B1101 << 1, 0B11010);
         // 1073741824 << 1 => -2147483648
         assertAllEquals(
-            0B0100_0000_0000_0000_0000_0000_0000_0000 << 1,
-            0B1000_0000_0000_0000_0000_0000_0000_0000,
-            -2147483648,
-            Integer.MIN_VALUE
+                0B0100_0000_0000_0000_0000_0000_0000_0000 << 1,
+                0B1000_0000_0000_0000_0000_0000_0000_0000,
+                -2147483648,
+                Integer.MIN_VALUE
         );
         assertAllEquals(0B0100_0000_0000_0000_0000_0000_0000_0000 << 2, 0);
         // 2147483647 << 1 => -4294967294 => -2
@@ -70,8 +70,67 @@ public class LangSampleTest {
             return true;
         }
         for (int i = 1; i < nums.length; i++) {
-            Assert.assertTrue(nums[0] == nums[i]);
+            Assert.assertTrue(nums[0] + " != " + nums[i], nums[0] == nums[i]);
         }
         return true;
+    }
+
+    @Test
+    public void testHexIntRange() {
+        //// negative
+        assertAllEquals(-0xFFFF_FFFF, 0x0000_0001, 1);
+        assertAllEquals(-0x8000_0001, 0x7FFF_FFFF, Integer.MAX_VALUE);
+        // 符号突变
+        assertAllEquals(-0x8000_0000, 0x8000_0000, Integer.MIN_VALUE);
+        assertAllEquals(-0x7FFF_FFFF, 0x8000_0001, Integer.MIN_VALUE + 1);
+        assertAllEquals(-0x0000_0001, 0xFFFF_FFFF);
+        assertAllEquals(-0x0000_0000, 0);
+        //// positive
+        assertAllEquals(0x0000_0000, 0);
+        assertAllEquals(0x0000_0001, 1);
+        assertAllEquals(0x7FFF_FFFF, 2147483647, Integer.MAX_VALUE);
+        // 符号突变
+        assertAllEquals(0x8000_0000, -2147483648, Integer.MIN_VALUE);
+        assertAllEquals(0x8000_0001, -2147483647, -Integer.MAX_VALUE);
+        assertAllEquals(0xFFFF_FFFF, -1);
+    }
+
+    @Test
+    public void testParseHexInt() {
+        // Integer.parseInt("-80000001", 16)： NumberFormatException
+        assertAllEquals(Integer.parseInt("-80000000", 16), 0x8000_0000, Integer.MIN_VALUE);
+        assertAllEquals(Integer.parseInt("-7FFFFFFF", 16), -0x7FFF_FFFF, 0x8000_0001);
+        assertAllEquals(Integer.parseInt("-00000001", 16), -0x0000_0001, 0xFFFF_FFFF);
+        assertAllEquals(Integer.parseInt("-00000000", 16), 0x0000_0000);
+        assertAllEquals(Integer.parseInt("00000000", 16), 0x0000_0000);
+        assertAllEquals(Integer.parseInt("00000001", 16), 0x0000_0001);
+        assertAllEquals(Integer.parseInt("0000000F", 16), 0x0000_000F);
+        assertAllEquals(Integer.parseInt("00000010", 16), 0x0000_0010);
+        assertAllEquals(Integer.parseInt("7FFFFFFF", 16), 0x7FFF_FFFF, Integer.MAX_VALUE);
+        // Integer.parseInt("80000000", 16)： NumberFormatException
+    }
+
+    @Test
+    public void testToString() {
+        Assert.assertEquals(Integer.toString(0x0000_0000, 10), "0");
+        Assert.assertEquals(Integer.toString(0x7FFF_FFFF, 10), "2147483647");
+        Assert.assertEquals(Integer.toString(0x8000_0000, 10), "-2147483648");
+        Assert.assertEquals(Integer.toString(0xFFFF_FFFF, 10), "-1");
+    }
+
+    @Test
+    public void testToHexString() {
+        Assert.assertEquals(Integer.toHexString(0x0000_0000), "0");
+        Assert.assertEquals(Integer.toHexString(0x7FFF_FFFF), "7fffffff");
+        Assert.assertEquals(Integer.toHexString(0x8000_0000), "80000000");
+        Assert.assertEquals(Integer.toHexString(0xFFFF_FFFF), "ffffffff");
+    }
+
+    @Test
+    public void testToUnsignedString() {
+        Assert.assertEquals(Integer.toUnsignedString(0x0000_0000), "0");
+        Assert.assertEquals(Integer.toUnsignedString(0x7FFF_FFFF), "2147483647");
+        Assert.assertEquals(Integer.toUnsignedString(0x8000_0000), "2147483648");
+        Assert.assertEquals(Integer.toUnsignedString(0xFFFF_FFFF), "4294967295");
     }
 }
